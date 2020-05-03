0da7360e5c498bf925c59144792f9a46f43ba168
tarasyi <dirigablenorge@gmail.com>
2020-05-03 16:18:32 +0400

    add svg sprite and small decorative elements

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const sourcemap = require('gulp-sourcemaps');
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const csso = require('gulp-csso');
-const server = require("browser-sync").create();
-const rename = require("gulp-rename");
-const uglify = require("gulp-uglify");
-const del = require("del");
+const server = require('browser-sync').create();
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const svgo = require('gulp-svgo');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
 
 gulp.task('css', () => {
   return gulp
@@ -40,11 +42,11 @@ gulp.task('js-min', () => {
       uglify()
     )
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest('build/js'));
 })
 
 gulp.task('clean', () => {
-  return del("build");
+  return del('build');
 });
 
 gulp.task('copy', function() {
@@ -52,7 +54,7 @@ gulp.task('copy', function() {
     .src(
       [
         'source/*.html',
-        'source/img/**/*{png,jpg,svg}',
+        'source/img/**/*{png,jpg}',
         'source/fonts/**/*.{woff,woff2}',
         'source/*.ico'
       ],
@@ -60,7 +62,7 @@ gulp.task('copy', function() {
         base: 'source'
       }
     )
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy-html', function() {
@@ -73,7 +75,26 @@ gulp.task('copy-html', function() {
         base: 'source'
       }
     )
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('svgo', () => {
+  return gulp
+  .src('source/img/*.svg')
+  .pipe(svgo({removeStyleElement: true}))
+  .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('svg-sprite', () => {
+  return gulp
+    .src('build/img/icon-*.svg')
+    .pipe(
+      svgstore({
+        inlineSvg: true
+      })
+    )
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('server', () => {
@@ -95,9 +116,9 @@ gulp.task('server', () => {
   gulp.watch("source/js/*.js", gulp.series('js-min', 'refresh'));
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'css-min', 'copy-html', 'js-min'));
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'css-min', 'copy', 'js-min', 'svgo', 'svg-sprite'));
 gulp.task('start', gulp.series('build', 'server'));
 
 
-gulp.task('dev-build', gulp.series('css', 'css-min', 'copy', 'copy-html', 'js-min'));
+gulp.task('dev-build', gulp.series('css', 'css-min', 'copy-html', 'js-min'));
 gulp.task('dev-start', gulp.series('dev-build', 'server'));
