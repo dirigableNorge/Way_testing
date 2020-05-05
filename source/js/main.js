38780ff9fdfce1ecf758e224cc216a8e940bde3c
tarasyi <dirigablenorge@gmail.com>
2020-05-05 14:08:12 +0400

    add open and close menu events

---
+++ source/js/main.js
@@ -0,0 +1,29 @@
+//menu
+
+const headMenu = document.querySelector('.page-header__nav-list');
+const headMenuOpenButton = document.querySelector('.page-header__nav-open-button');
+const headMenuCloseButton = document.querySelector('.page-header__nav-close-button');
+
+const onOpenHeadMenu = () => {
+  headMenu.classList.remove('page-header__nav-list--close');
+  headMenuCloseButton.classList.remove('page-header__nav-close-button--close');
+};
+
+const onCloseHeadMenu = () => {
+  console.log(headMenu);
+  headMenu.classList.add('page-header__nav-list--close');
+  headMenuCloseButton.classList.add('page-header__nav-close-button--close');
+};
+
+
+if (headMenuOpenButton) {
+  headMenuOpenButton.addEventListener('click', onOpenHeadMenu);
+}
+
+if (headMenuCloseButton) {
+  headMenuCloseButton.addEventListener('click', onCloseHeadMenu);
+}
+
+window.onload = () => {
+  onCloseHeadMenu();
+};
