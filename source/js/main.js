86826c5dc230f1e4e5f38e16a1ea9093adaa44fa
tarasyi <dirigablenorge@gmail.com>
2020-05-05 21:12:46 +0400

    add mobile styles for modal

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,18 @@
 //menu
 
-const headMenu = document.querySelector('.page-header__nav-list');
+const headMenuElement = document.querySelector('.page-header__nav-list');
 const headMenuOpenButton = document.querySelector('.page-header__nav-open-button');
 const headMenuCloseButton = document.querySelector('.page-header__nav-close-button');
 
 const onOpenHeadMenu = () => {
-  headMenu.classList.remove('page-header__nav-list--close');
+  headMenuElement.classList.remove('page-header__nav-list--close');
   headMenuCloseButton.classList.remove('page-header__nav-close-button--close');
 };
 
 const onCloseHeadMenu = () => {
-  headMenu.classList.add('page-header__nav-list--close');
+  headMenuElement.classList.add('page-header__nav-list--close');
   headMenuCloseButton.classList.add('page-header__nav-close-button--close');
 };
-
 
 if (headMenuOpenButton) {
   headMenuOpenButton.addEventListener('click', onOpenHeadMenu);
@@ -26,3 +25,25 @@ if (headMenuCloseButton) {
 window.onload = () => {
   onCloseHeadMenu();
 };
+
+// modal
+
+const buyTourModalElement = document.querySelector('.buy-tour-modal');
+const buyTourModalCLoseButton = document.querySelector('.buy-tour-modal__close-button');
+const buyTourModalOpenButton = document.querySelector('.buy-tour-modal-open-button');
+
+const onOpenBuyTourModal = () => {
+  buyTourModalElement.classList.remove('hidden');
+}
+
+const onCloseBuyTourModal = () => {
+  buyTourModalElement.classList.add('hidden');
+}
+
+if (buyTourModalCLoseButton) {
+  buyTourModalCLoseButton.addEventListener('click', onCloseBuyTourModal)
+}
+
+if (buyTourModalOpenButton) {
+  buyTourModalOpenButton.addEventListener('click', onOpenBuyTourModal);
+}
